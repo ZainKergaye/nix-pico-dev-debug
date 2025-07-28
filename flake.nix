3420ce6f5c708e386810ed1740a3dcf3253b9bc2
Zain Kergaye <zain4utah@gmail.com>
2025-07-28 16:04:06 -0600

    Compiled blink

--- flake.nix
+++ flake.nix
@@ -2,94 +2,50 @@
   description = "A very basic flake with picoprobe build capabilities";
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
-    picoprobe = {
-      flake = false;
-      url = "github:raspberrypi/picoprobe";
-    };
-    freeRTOS-kernel = {
-      flake = false;
-      url = "github:FreeRTOS/FreeRTOS-kernel";
-    };
+    flake-utils.url = "github:numtide/flake-utils";
   };
 
   outputs =
     { self
     , nixpkgs
-    , picoprobe
-    , freeRTOS-kernel
+    , flake-utils
     ,
     }:
-    let
-      pkgs = nixpkgs.legacyPackages."x86_64-linux";
-
-      freeRTOSKernel = pkgs.fetchFromGitHub {
-        owner = "FreeRTOS";
-        repo = "FreeRTOS-Kernel";
-        rev = "0030d609a4b99118d9a400340d88c3c3c4816f2b"; # The specific commit hash
-        sha256 = "sha256-uiAXeuyE40fMf14dfgf0WW3agd+93L/akfABWcXfOjA=";
-      };
-
-      # Define the picoprobe package
-      picoprobePackage = pkgs.stdenv.mkDerivation {
-        name = "picoprobe";
-        src = picoprobe;
-
-        buildInputs = with pkgs; [
-          cmake
-          gcc-arm-embedded # Required for ARM cross-compilation
-          gnumake
-          pico-sdk
-          libtool
-          automake
-          libusb
-          wget
-          pkg-config
-          gcc
-          texinfo
-        ];
-
-        env = {
-          DPICO_SDK_PATH = "${pkgs.pico-sdk}/lib/pico-sdk";
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = nixpkgs.legacyPackages."x86_64-linux";
+        pico-sdk-211 = with pkgs; (pico-sdk.overrideAttrs (o: rec {
+          pname = "pico-sdk";
+          version = "2.1.1";
+          src = fetchFromGitHub {
+            fetchSubmodules = true; # TinyUSB not included in store package
+            owner = "raspberrypi";
+            repo = pname;
+            rev = version;
+            sha256 = "sha256-8ru1uGjs11S2yQ+aRAvzU53K8mreZ+CC3H+ijfctuqg=";
+          };
+        }));
+      in
+      with pkgs; {
+        devShell = mkShell {
+          buildInputs = [
+            openocd-rp2040
+            cmake
+            python3
+            gcc-arm-embedded
+            pico-sdk-211
+            picotool
+          ];
+          PICO_SDK_PATH = "${pico-sdk}/lib/pico-sdk";
           PICO_BOARD = "pico";
-          DFREERTOS_DIR = "${freeRTOSKernel}";
+          PICO_PLATFORM = "rp2040";
+          PICO_COMPILER = "pico_arm_cortex_m0plus_gcc";
         };
 
-        configurePhase = ''
-                 mkdir -p $out/build
-                 cd $out/build
-                 cmake . -G "Unix Makefiles" \
-                   -DCMAKE_BUILD_TYPE=Release \
-                   -DFREERTOS_DIR=${freeRTOSKernel} \
-          -DPICO_SDK_PATH=${pkgs.pico-sdk}/lib/pico-sdk
-        '';
-
-        buildPhase = ''
-          cd $out/build
-          make
-        '';
-
-        installPhase = ''
-          mkdir -p $out/bin
-          cp $out/build/probe $out/bin/
-        '';
-      };
-    in
-    {
-      devShells."x86_64-linux".default = pkgs.mkShell {
-        packages = with pkgs; [
-          openocd-rp2040
-          cmake
-          python3
-          gcc-arm-embedded
-          newlib
-        ];
         shellHook = ''
           echo "Welcome to the pico debug devshell"
         '';
-      };
-
-      # Add an output for the picoprobe package itself
-      packages."x86_64-linux".picoprobe = picoprobePackage;
-      packages.x86_64-linux.default = self.packages.x86_64-linux.picoprobe;
-    };
+      }
+    );
 }
